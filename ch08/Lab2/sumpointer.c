e0097e338f7234d1bb33d1ef1c8b61ac3cdc4914
ckswhd200 <63796128+ckswhd200@users.noreply.github.com>
2020-12-08 10:24:46 +0900

    Add files via upload

---
+++ ch08/Lab2/sumpointer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+	double x, y;
+	double* px = &x;
+	double* py = &y;
+
+	printf("두 실수 입력: ");
+	scanf_s("%lf %lf", px, py);
+	printf("%.2f + %.2f = %.2f\n", *px, *py, *px + *py);
+
+	return 0;
+}
---
+++ ch08/Prj02/nullpointer.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+	int* ptr1, * ptr2, data = 10;
+	ptr1 = NULL;
+
+	printf("%p\n", ptr1);
+	//printf("%p\n", ptr2);
+	printf("%d\n", data);
+	return 0;
+}
